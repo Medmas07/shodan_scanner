be49cf01f751bf280b6b7404ebfd5cae4f7ae7b7
Medmas07 <mohamed.masmoudi0712@gmail.com>
2025-07-31 13:27:34 +0100

    a small change

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "shodan-scan=scanner.cli:main",  # commande terminale → fonction à appeler
+            "shodan-scan=cli:main",  # commande terminale → fonction à appeler
         ]
     },
     author="WhoCare",
